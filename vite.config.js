88c63386b16e3322e5f394ec2aaee810c1105722
Emmanuel Kumah <mailkumah@gmail.com>
2023-06-20 22:02:43 +0000

    Update base url in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "articleoutline_gen",
+  base: "/articleoutline_gen/",
 });
